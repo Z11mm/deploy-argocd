244f654faa47132beef4fb00f13a6281d14ac1c7
Ziimm <zimuzo.obiechina@deimos.co.za>
2022-02-02 14:14:54 +0100

    Fix:Change cluster module

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,7 +29,7 @@ module "gke_cluster" {
   # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
   # to a specific version of the modules, such as the following example:
   # source = "github.com/gruntwork-io/terraform-google-gke.git//modules/gke-cluster?ref=v0.2.0"
-  source = "terraform-google-modules/kubernetes-engine/google/"
+  source = "https://github.com/terraform-google-modules/terraform-google-kubernetes-engine.git"
 
   region                     = var.region
   # zones                      = ["us-central1-a", "us-central1-b", "us-central1-f"]
