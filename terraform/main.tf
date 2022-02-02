718b4539ff7953c320d362163c1779abcc054c02
Ziimm <zimuzo.obiechina@deimos.co.za>
2022-02-02 15:18:06 +0100

    Fix:Add cluster resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,31 +25,31 @@ module "vpc_network" {
 # --------------------------------------------------------------------------------------
 # CREATE GKE CLUSTER
 # --------------------------------------------------------------------------------------
-module "gke_cluster" {
-  # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
-  # to a specific version of the modules, such as the following example:
-  # source = "github.com/gruntwork-io/terraform-google-gke.git//modules/gke-cluster?ref=v0.2.0"
-  source = "github.com/gruntwork-io/terraform-google-gke.git//modules/gke-cluster?ref=v0.10.0"
-  # version = "19.0.0"
-   name = "${var.cluster_name}-${var.environment}"
+# module "gke_cluster" {
+#   # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
+#   # to a specific version of the modules, such as the following example:
+#   # source = "github.com/gruntwork-io/terraform-google-gke.git//modules/gke-cluster?ref=v0.2.0"
+#   source = "github.com/gruntwork-io/terraform-google-gke.git//modules/gke-cluster?ref=v0.10.0"
+#   # version = "19.0.0"
+#    name = "${var.cluster_name}-${var.environment}"
 
-  project  = var.project
-  location = var.region
+#   project  = var.project
+#   location = var.region
 
-  # We're deploying the cluster in the 'public' subnetwork to allow outbound internet access
-  # See the network access tier table for full details:
-  # https://github.com/gruntwork-io/terraform-google-network/tree/master/modules/vpc-network#access-tier
-  network                      = module.vpc_network.network
-  subnetwork                   = module.vpc_network.public_subnetwork
-  cluster_secondary_range_name = module.vpc_network.public_subnetwork_secondary_range_name
+#   # We're deploying the cluster in the 'public' subnetwork to allow outbound internet access
+#   # See the network access tier table for full details:
+#   # https://github.com/gruntwork-io/terraform-google-network/tree/master/modules/vpc-network#access-tier
+#   network                      = module.vpc_network.network
+#   subnetwork                   = module.vpc_network.public_subnetwork
+#   cluster_secondary_range_name = module.vpc_network.public_subnetwork_secondary_range_name
 
-  # To make testing easier, we keep the public endpoint available. In production, we highly recommend restricting access to only within the network boundary, requiring your users to use a bastion host or VPN.
-  disable_public_endpoint = "false"
+#   # To make testing easier, we keep the public endpoint available. In production, we highly recommend restricting access to only within the network boundary, requiring your users to use a bastion host or VPN.
+#   disable_public_endpoint = "false"
 
-  # add resource labels to the cluster
-  resource_labels = {
-    environment = var.environment
-  }
+#   # add resource labels to the cluster
+#   resource_labels = {
+#     environment = var.environment
+#   }
 
   # region                     = var.region
   # ip_range_pods              = var.ip_range_pods
@@ -74,6 +74,22 @@ module "gke_cluster" {
   # resource_labels = {
   #   environment = var.environment
   # }
+# }
+resource "google_container_cluster" "main" {
+  name               = "${var.cluster_name}"
+  location           = var.region
+  initial_node_count = 1
+  # node_config {
+  #   # service_account = google_service_account.main.email
+  #   oauth_scopes = [
+  #     "https://www.googleapis.com/auth/cloud-platform"
+  #   ]
+  # }
+  remove_default_node_pool = true
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
 }
 
 # #--------------------------------------------------------------------------------------
@@ -82,8 +98,8 @@ module "gke_cluster" {
 resource "google_container_node_pool" "node-pool" {
   # provider = google-beta
 
-  name       = "${module.gke_cluster.name}-node-pool"
-  cluster    = module.gke_cluster.name
+  name       = "${var.cluster_name}-node-pool"
+  cluster    = var.cluster_name
   location   = var.region
   node_count = 1
 
@@ -120,7 +136,7 @@ module "gke_auth" {
   depends_on           = [time_sleep.wait_30_seconds]
   source               = "terraform-google-modules/kubernetes-engine/google//modules/auth"
   project_id           = var.project_id
-  cluster_name         = module.gke_cluster.name
+  cluster_name         = var.cluster_name
   location             = var.region
   use_private_endpoint = false
 }
