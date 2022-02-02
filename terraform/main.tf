1c1fdb31a6211482655ff0404c9042468e2822a3
Ziimm <zimuzo.obiechina@deimos.co.za>
2022-02-02 15:12:19 +0100

    Fix:update module reference

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,23 +29,42 @@ module "gke_cluster" {
   # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
   # to a specific version of the modules, such as the following example:
   # source = "github.com/gruntwork-io/terraform-google-gke.git//modules/gke-cluster?ref=v0.2.0"
-  source = "/github.com/Z11mm/terraform-google-kubernetes-engine/"
-  version = "19.0.0"
+  source = "github.com/gruntwork-io/terraform-google-gke.git//modules/gke-cluster?ref=v0.10.0"
+  # version = "19.0.0"
+   name = "${var.cluster_name}-${var.environment}"
 
-  region                     = var.region
-  ip_range_pods              = var.ip_range_pods
-  ip_range_services          = var.ip_range_services
-  http_load_balancing        = false
-  # horizontal_pod_autoscaling = true
-  name = "${var.cluster_name}"
-
-  project_id  = var.project
+  project  = var.project
+  location = var.region
 
   # We're deploying the cluster in the 'public' subnetwork to allow outbound internet access
   # See the network access tier table for full details:
   # https://github.com/gruntwork-io/terraform-google-network/tree/master/modules/vpc-network#access-tier
-  network                      = "${var.network}-${random_string.suffix.result}-${var.environment}"
+  network                      = module.vpc_network.network
   subnetwork                   = module.vpc_network.public_subnetwork
+  cluster_secondary_range_name = module.vpc_network.public_subnetwork_secondary_range_name
+
+  # To make testing easier, we keep the public endpoint available. In production, we highly recommend restricting access to only within the network boundary, requiring your users to use a bastion host or VPN.
+  disable_public_endpoint = "false"
+
+  # add resource labels to the cluster
+  resource_labels = {
+    environment = var.environment
+  }
+
+  # region                     = var.region
+  # ip_range_pods              = var.ip_range_pods
+  # ip_range_services          = var.ip_range_services
+  # http_load_balancing        = false
+  # horizontal_pod_autoscaling = true
+  # name = "${var.cluster_name}"
+
+  # project_id  = var.project
+
+  # We're deploying the cluster in the 'public' subnetwork to allow outbound internet access
+  # See the network access tier table for full details:
+  # https://github.com/gruntwork-io/terraform-google-network/tree/master/modules/vpc-network#access-tier
+  # network                      = "${var.network}-${random_string.suffix.result}-${var.environment}"
+  # subnetwork                   = module.vpc_network.public_subnetwork
   # cluster_secondary_range_name = module.vpc_network.public_subnetwork_secondary_range_name
 
   # To make testing easier, we keep the public endpoint available. In production, we highly recommend restricting access to only within the network boundary, requiring your users to use a bastion host or VPN.
